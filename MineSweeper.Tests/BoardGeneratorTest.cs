651e85e98a92cd43f729ddb54a47ae271f0b591d
Emili <emiliisamat@gmail.com>
2020-10-12 19:48:48 +0200

    GENERATE BOARD: Added tests

---
+++ MineSweeper.Tests/BoardGeneratorTest.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+
+namespace MineSweeper.Tests
+{
+    [TestFixture]
+    public class BoardGeneratorTest
+    {
+        [Test]
+        public void TestCorrectBoardSize()
+        {
+            var boardOptions = new BoardOptions(10, 10);
+            var board = new BoardGenerator().GenerateBoard(boardOptions);
+            Assert.AreEqual(10 * 10, board.Size);
+            Assert.AreEqual(10, board.SideSize);
+        }
+
+        [Test]
+        public void TestCorrectMineNumber()
+        {
+            var boardOptions = new BoardOptions(10, 10);
+            var board = new BoardGenerator().GenerateBoard(boardOptions);
+            Assert.AreEqual(10, board.MineNumber);
+        }
+
+        [Test]
+        public void TestSameSeedSameBoard()
+        {
+            const long seed = 123456789L;
+
+            var generator = new BoardGenerator();
+            var board = generator.GenerateBoard(new BoardOptions(10, 10, seed));
+            Assert.AreEqual(board, generator.GenerateBoard(new BoardOptions(10, 10, seed)));
+        }
+    }
+}
---
+++ MineSweeper/Game/BoardGenerator.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+namespace MineSweeper
+{
+    public class BoardCell
+    {
+        public readonly bool IsMine;
+        public readonly int NeighbouringCells;
+        public readonly int X;
+        public readonly int Y;
+
+        public BoardCell(int x, int y, bool isMine, int neighbouringCells)
+        {
+            X = x;
+            Y = y;
+            IsMine = isMine;
+            NeighbouringCells = neighbouringCells;
+        }
+    }
+
+    public class Board
+    {
+        private readonly BoardCell[] Cells;
+        public readonly int MineNumber;
+        public readonly int Size;
+        public readonly int SideSize;
+
+        public Board(int mineNumber, int size, BoardCell[] cells)
+        {
+            MineNumber = mineNumber;
+            SideSize = size;
+            Size = size * size;
+            Cells = cells;
+        }
+
+    }
+
+    public class BoardOptions
+    {
+        private readonly int Mines;
+        private readonly long Seed;
+        private readonly int Size;
+
+        public BoardOptions(int size, int mines, long seed)
+        {
+            Size = size;
+            Mines = mines;
+            Seed = seed;
+        }
+
+        public BoardOptions(int size, int mines) : this(size, mines, Environment.TickCount)
+        {
+            
+        }
+    }
+
+    public interface IBoardGenerator
+    {
+        Board GenerateBoard(BoardOptions options);
+    }
+
+    public class BoardGenerator : IBoardGenerator
+    {
+        public Board GenerateBoard(BoardOptions options)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
