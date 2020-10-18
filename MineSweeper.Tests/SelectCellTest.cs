95ecb30bfa27b3702b4bf9eb3b6365bf06e911e9
Emili <emiliisamat@gmail.com>
2020-10-18 17:20:15 +0200

    SELECT CELL: Added tests for the select cell behaviour. Created the game manager exceptions.

---
+++ MineSweeper.Tests/SelectCellTest.cs
@@ -0,0 +1,162 @@
+﻿using System.Numerics;
+using MineSweeper.Game;
+using MineSweeper.Game.Models;
+using NUnit.Framework;
+
+namespace MineSweeper.Tests
+{
+    [TestFixture]
+    public class SelectCellTest
+    {
+        private GameManager gameManager;
+        
+        [SetUp]
+        public void SetUp()
+        {
+            gameManager = new GameManager();    
+        }
+        
+        /// <summary>
+        /// Test that the manager throws an exception when the user tries to select an invalid
+        /// position inside the board.
+        /// </summary>
+        [Test]
+        public void TestInvalidPositionThrowsException()
+        {
+            var cells = new[]
+            {
+                new BoardCell { Position = new Vector2(0, 0), IsMine = false, NeighbouringCells = 0, Status = CellStatus.VISIBLE},
+            };
+            
+            var board = new Board(1, new Vector2(1, 1), cells);
+            
+            Assert.Throws<InvalidBoardPositionException>(() => gameManager.SelectCell(board, new Vector2(10, 10)));
+        }
+        
+        /// <summary>
+        /// Test that the manager throws an exception when the user tries to select an already visible
+        /// cell.
+        /// </summary>
+        [Test]
+        public void TestVisibleCellThrowsException()
+        {
+            var cells = new[]
+            {
+                new BoardCell { Position = new Vector2(0, 0), IsMine = false, NeighbouringCells = 0, Status = CellStatus.VISIBLE},
+            };
+            
+            var board = new Board(1, new Vector2(1, 1), cells);
+            
+            Assert.Throws<CellAlreadyVisibleException>(() => gameManager.SelectCell(board, new Vector2(0, 0)));
+        }
+        
+        /// <summary>
+        /// Test that the manager throws an exception when the user selects a cell that contains a mine.
+        /// </summary>
+        [Test]
+        public void TestMinedCellThrowsException()
+        {
+            var cells = new[]
+            {
+                new BoardCell { Position = new Vector2(0, 0), IsMine = true, NeighbouringCells = 0, Status = CellStatus.HIDDEN},
+            };
+            
+            var board = new Board(1, new Vector2(1, 1), cells);
+            
+            Assert.Throws<MineFoundException>(() => gameManager.SelectCell(board, new Vector2(0, 0)));
+        }
+
+                
+        /// <summary>
+        /// Test that the manager marks the selected cell and all the neighbouring cells recursively that does not have
+        /// mined neighbouring cells as Visible.
+        /// </summary>
+        [Test]
+        public void TestValidPositionChangeCellsToVisibleState()
+        {
+            // [█][█][█]
+            // [█][█][█]
+            // [█ = *][█][█]
+            var initialCells = new[]
+            {
+                new BoardCell { Position = new Vector2(0, 0), IsMine = false, NeighbouringCells = 0, Status = CellStatus.HIDDEN},
+                new BoardCell { Position = new Vector2(1, 0), IsMine = false, NeighbouringCells = 0, Status = CellStatus.HIDDEN},
+                new BoardCell { Position = new Vector2(2, 0), IsMine = false, NeighbouringCells = 0, Status = CellStatus.HIDDEN},
+                new BoardCell { Position = new Vector2(0, 2), IsMine = false, NeighbouringCells = 1, Status = CellStatus.HIDDEN},
+                new BoardCell { Position = new Vector2(1, 2), IsMine = false, NeighbouringCells = 1, Status = CellStatus.HIDDEN},
+                new BoardCell { Position = new Vector2(2, 2), IsMine = false, NeighbouringCells = 0, Status = CellStatus.HIDDEN},
+                new BoardCell { Position = new Vector2(0, 3), IsMine = true, NeighbouringCells = 0, Status = CellStatus.HIDDEN},
+                new BoardCell { Position = new Vector2(1, 3), IsMine = false, NeighbouringCells = 1, Status = CellStatus.HIDDEN},
+                new BoardCell { Position = new Vector2(2, 3), IsMine = false, NeighbouringCells = 0, Status = CellStatus.HIDDEN},
+            };
+            
+            // [░][░][░]
+            // [1][1][░]
+            // [█][1][░]
+            var expectedCells = new[]
+            {
+                new BoardCell { Position = new Vector2(0, 0), IsMine = false, NeighbouringCells = 0, Status = CellStatus.VISIBLE},
+                new BoardCell { Position = new Vector2(1, 0), IsMine = false, NeighbouringCells = 0, Status = CellStatus.VISIBLE},
+                new BoardCell { Position = new Vector2(2, 0), IsMine = false, NeighbouringCells = 0, Status = CellStatus.VISIBLE},
+                new BoardCell { Position = new Vector2(0, 2), IsMine = false, NeighbouringCells = 1, Status = CellStatus.VISIBLE},
+                new BoardCell { Position = new Vector2(1, 2), IsMine = false, NeighbouringCells = 1, Status = CellStatus.VISIBLE},
+                new BoardCell { Position = new Vector2(2, 2), IsMine = false, NeighbouringCells = 0, Status = CellStatus.VISIBLE},
+                new BoardCell { Position = new Vector2(0, 3), IsMine = true, NeighbouringCells = 0, Status = CellStatus.HIDDEN},
+                new BoardCell { Position = new Vector2(1, 3), IsMine = false, NeighbouringCells = 1, Status = CellStatus.VISIBLE},
+                new BoardCell { Position = new Vector2(2, 3), IsMine = false, NeighbouringCells = 0, Status = CellStatus.VISIBLE},
+            };
+            
+            var board = new Board(1, new Vector2(3, 3), initialCells);
+       
+            gameManager.SelectCell(board, new Vector2(0, 0));
+            
+            Assert.Equals(expectedCells, board.Cells);
+        }
+        
+        /// <summary>
+        /// Test that the manager marks the selected cell and all the neighbouring cells recursively that does not have
+        /// mined neighbouring cells as Visible.
+        /// </summary>
+        [Test]
+        public void TestValidPositionChangeCellsToVisibleState2()
+        {
+            // [█][█][█]
+            // [█][█][█]
+            // [█ = *][█][█ = *]
+            var initialCells = new[]
+            {
+                new BoardCell { Position = new Vector2(0, 0), IsMine = false, NeighbouringCells = 0, Status = CellStatus.HIDDEN},
+                new BoardCell { Position = new Vector2(1, 0), IsMine = false, NeighbouringCells = 0, Status = CellStatus.HIDDEN},
+                new BoardCell { Position = new Vector2(2, 0), IsMine = false, NeighbouringCells = 0, Status = CellStatus.HIDDEN},
+                new BoardCell { Position = new Vector2(0, 2), IsMine = false, NeighbouringCells = 1, Status = CellStatus.HIDDEN},
+                new BoardCell { Position = new Vector2(1, 2), IsMine = false, NeighbouringCells = 2, Status = CellStatus.HIDDEN},
+                new BoardCell { Position = new Vector2(2, 2), IsMine = false, NeighbouringCells = 1, Status = CellStatus.HIDDEN},
+                new BoardCell { Position = new Vector2(0, 3), IsMine = true, NeighbouringCells = 0, Status = CellStatus.HIDDEN},
+                new BoardCell { Position = new Vector2(1, 3), IsMine = false, NeighbouringCells = 2, Status = CellStatus.HIDDEN},
+                new BoardCell { Position = new Vector2(2, 3), IsMine = true, NeighbouringCells = 0, Status = CellStatus.HIDDEN},
+            };
+            
+            // [░][░][░]
+            // [1][2][1]
+            // [█][█][█]
+            var expectedCells = new[]
+            {
+                new BoardCell { Position = new Vector2(0, 0), IsMine = false, NeighbouringCells = 0, Status = CellStatus.VISIBLE},
+                new BoardCell { Position = new Vector2(1, 0), IsMine = false, NeighbouringCells = 0, Status = CellStatus.VISIBLE},
+                new BoardCell { Position = new Vector2(2, 0), IsMine = false, NeighbouringCells = 0, Status = CellStatus.VISIBLE},
+                new BoardCell { Position = new Vector2(0, 2), IsMine = false, NeighbouringCells = 1, Status = CellStatus.VISIBLE},
+                new BoardCell { Position = new Vector2(1, 2), IsMine = false, NeighbouringCells = 2, Status = CellStatus.VISIBLE},
+                new BoardCell { Position = new Vector2(2, 2), IsMine = false, NeighbouringCells = 1, Status = CellStatus.VISIBLE},
+                new BoardCell { Position = new Vector2(0, 3), IsMine = true, NeighbouringCells = 0, Status = CellStatus.HIDDEN},
+                new BoardCell { Position = new Vector2(1, 3), IsMine = false, NeighbouringCells = 2, Status = CellStatus.HIDDEN},
+                new BoardCell { Position = new Vector2(2, 3), IsMine = true, NeighbouringCells = 0, Status = CellStatus.HIDDEN},
+            };
+            
+            var board = new Board(1, new Vector2(3, 3), initialCells);
+       
+            gameManager.SelectCell(board, new Vector2(0, 0));
+            
+            Assert.Equals(expectedCells, board.Cells);
+        }
+    }
+}
---
+++ MineSweeper/Game/GameManager.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Numerics;
+using MineSweeper.Game.Models;
+
+namespace MineSweeper.Game
+{
+    public class CellAlreadyVisibleException: Exception
+    {
+        private BoardCell cell;
+
+        public CellAlreadyVisibleException(BoardCell cell): base($"This cell is already visible")
+        {
+            this.cell = cell;
+        }
+    }
+    
+    public class MineFoundException: Exception
+    {
+        private BoardCell cell;
+
+        public MineFoundException(BoardCell cell): base($"You selected a mined cell")
+        {
+            this.cell = cell;
+        }
+    }
+    
+    public class InvalidBoardPositionException: Exception
+    {
+        private Vector2 position;
+
+        public InvalidBoardPositionException(Vector2 position): base($"This position is invalid: {position}")
+        {
+            this.position = position;
+        }
+    }
+    
+    public interface IGameManager
+    {
+        void SelectCell(Board board, Vector2 position);
+    }
+
+    public class GameManager: IGameManager
+    {
+        public void SelectCell(Board board, Vector2 position)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
