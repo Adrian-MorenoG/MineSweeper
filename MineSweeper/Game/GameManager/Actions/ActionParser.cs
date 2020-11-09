ee951d1d0cd8cdb8606710c3745986272b646501
eisamat <emiliisamat@gmail.com>
2020-11-09 12:57:41 +0100

    SELECT CELL ACTION: Added implementation

--- MineSweeper/Game/GameManager/Actions/ActionParser.cs
+++ MineSweeper/Game/GameManager/Actions/ActionParser.cs
@@ -1,4 +1,7 @@
-﻿namespace MineSweeper.Game.GameManager.Actions
+﻿using System;
+using System.Numerics;
+
+namespace MineSweeper.Game.GameManager.Actions
 {
     public interface IActionParser
     {
@@ -9,7 +12,33 @@
     {
         public Action ParseAction(string input)
         {
-            throw new System.NotImplementedException();
+            var parts = input.Split(" ");
+
+            return parts[0] switch
+            {
+                "S" => ParseSelectCellAction(parts),
+                _ => throw new ArgumentException("Invalid action")
+            };
+        }
+
+        private Action ParseSelectCellAction(string[] parts)
+        {
+            if (parts.Length != 3)
+            {
+                throw new ArgumentException("Invalid action");
+            }
+
+            try
+            {
+                var x = Convert.ToInt32(parts[1]);
+                var y = Convert.ToInt32(parts[2]);
+
+                return new SelectCellAction(new Vector2(x, y));
+            }
+            catch (Exception e)
+            {
+                throw new ArgumentException("Invalid action");
+            }
         }
     }
 }
